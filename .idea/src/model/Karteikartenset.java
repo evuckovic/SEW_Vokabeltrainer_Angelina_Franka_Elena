edbb13d20b3ca6c70432bed00514961f64159d5b
aglatzl <aglatzl@student.tgm.ac.at>
2026-02-10 11:26:08 +0100

    Karteikarten Klassen erstellt

---
+++ .idea/src/model/Karteikartenset.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Karteikartenset extends Karteikarte{
+    private String[] vokabelpaar;
+    private final int arraygröße = 10; //Jedes Karteikartenset hat maximal 10 Karteikarten
+}
