d025c4239f2aa6a8e28e4dc26c224d9e0b107eee
aglatzl <aglatzl@student.tgm.ac.at>
2026-02-12 08:05:27 +0100

    Weitere Klassen erstellt

--- .idea/src/model/Karteikarte.java
+++ .idea/src/model/Karteikarte.java
@@ -4,4 +4,11 @@ public class Karteikarte{
     private String[] vokabelpaar; //immer in dem Format gespeichert: deutsch, englisch
     private String name; //der Name der Karteikarte ist immer der Deutsche Begriff
 
+    public String[] getVokabelpaar() {
+        return vokabelpaar;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
