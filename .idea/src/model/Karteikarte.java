b95fcef042a09a4c45eaa9d066cb9519adb30bd6
aglatzl <aglatzl@student.tgm.ac.at>
2026-02-13 08:02:28 +0100

    Verbesserungen

--- .idea/src/model/Karteikarte.java
+++ .idea/src/model/Karteikarte.java
@@ -2,6 +2,8 @@ package model;
 
 public class Karteikarte{
     private String[] vokabelpaar = new String[2];  //immer in dem Format gespeichert: deutsch, englisch
+    //Bild zur Karteikarte als Link aus dem Internet
+
 
     public Karteikarte(String[] vokabelpaar, String name) {
         this.vokabelpaar = vokabelpaar;
@@ -11,7 +13,19 @@ public class Karteikarte{
         return vokabelpaar;
     }
 
+    public void setDeutsch(String deutsch){
+        if(deutsch != null && !deutsch.isEmpty()){
+            this.vokabelpaar[0] = deutsch; //erste stelle im array ist immer englisch
+        }
+    }
+
+    public void setEnglisch(String englisch){
+        if(englisch != null && !englisch.isEmpty()){
+            this.vokabelpaar[1] = englisch; //zweite stelle im array ist immer englisch
+        }
+    }
     public void setVokabelpaar(String[] vokabelpaar) {
         this.vokabelpaar = vokabelpaar;
     }
+
 }
